3113932170b5a7cbe73e3897075bc7a63641da59
Benard Asiimwe Bataringaya <benard.asiimwe.bataringaya@gmail.com>
2023-06-03 17:06:59 +0300

    drawer skeleton

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -1,48 +1,76 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
 import "./NavBar.scss";
 import { Link } from "react-router-dom";
-import Badge from '@mui/material/Badge';
+import Badge from "@mui/material/Badge";
 import { ShoppingCart } from "@mui/icons-material";
 
-const NavBar = () => (
-  <Navbar expand="lg" sticky="top">
-    <Container fluid>
-      <Navbar.Brand href="#">
-        <Image src="vite@2x.png" alt="Nans logo" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarScroll" />
-      <Navbar.Collapse id="navbarScroll">
-        <Nav
-          className="me-auto my-2 my-lg-0 justify-content-end"
-          style={{ width: "100%" }}
-          navbarScroll
-        >
-          <Link to="/" className="nav-link">
-            <Nav.Item>Home</Nav.Item>
-          </Link>
-          <Link to="/about" className="nav-link">
-            <Nav.Item>About</Nav.Item>
-          </Link>
-          <Link to="/contact" className="nav-link">
-            <Nav.Item>Contact</Nav.Item>
-          </Link>
-          <Link className="nav-link">
-            <Nav.Item>Login</Nav.Item>
-          </Link>
-          <Link className="nav-link">
-            <Nav.Item>
-              <Badge badgeContent={4} color="success">
-                <ShoppingCart color="light" />
-              </Badge>
-            </Nav.Item>
-          </Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+import Drawer from "@mui/material/Drawer";
+
+const NavBar = () => {
+  const [state, setState] = useState({
+    showDrawer: false,
+  });
+  const toggleDrawer = (e) => {
+    e.preventDefault();
+    setState((prev) => {
+      return {
+        ...prev,
+        showDrawer: !prev.showDrawer,
+      };
+    });
+  };
+
+  return (
+    <>
+      <Navbar expand="lg" sticky="top">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            <Image src="vite@2x.png" alt="Nans logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0 justify-content-end"
+              style={{ width: "100%" }}
+              navbarScroll
+            >
+              <Link to="/" className="nav-link">
+                <Nav.Item>Home</Nav.Item>
+              </Link>
+              <Link to="/about" className="nav-link">
+                <Nav.Item>About</Nav.Item>
+              </Link>
+              <Link to="/contact" className="nav-link">
+                <Nav.Item>Contact</Nav.Item>
+              </Link>
+              <Link className="nav-link">
+                <Nav.Item>Login</Nav.Item>
+              </Link>
+              <Link className="nav-link" onClick={(e) => toggleDrawer(e)}>
+                <Nav.Item>
+                  <Badge badgeContent={4} color="success">
+                    <ShoppingCart color="light" />
+                  </Badge>
+                </Nav.Item>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Drawer
+        anchor="right"
+        open={state.showDrawer}
+        onClose={(e) => toggleDrawer(e)}
+      >
+       <Container className="drawer-container">
+       </Container>
+      </Drawer>
+    </>
+  );
+};
 
 export default NavBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,56 +1,72 @@
+import { useRef } from "react";
 import { Row, Container } from "react-bootstrap";
 import "./Home.scss";
 import ProductList from "../components/products/ProductList";
 import background from "../assets/background.jpg";
+import Fab from "@mui/material/Fab";
+import { KeyboardArrowUp } from "@mui/icons-material";
 
 const data = [
   {
     id: 1,
-    imageSource:background,
-    title:"Product 1",
-    rating:5,
-    reviews:["review 1", "review 2"],
-    price:13000,
-    quantity:20,
-    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie ornare elit, in facilisis odio suscipit ut. Integer mollis quam id placerat auctor. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent vitae rhoncus sem, vel lobortis ipsum. In hac habitasse platea dictumst. Nulla porta volutpat nulla a vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
+    imageSource: background,
+    title: "Product 1",
+    rating: 5,
+    reviews: ["review 1", "review 2"],
+    price: 13000,
+    quantity: 20,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie ornare elit, in facilisis odio suscipit ut. Integer mollis quam id placerat auctor. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent vitae rhoncus sem, vel lobortis ipsum. In hac habitasse platea dictumst. Nulla porta volutpat nulla a vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
   },
   {
     id: 2,
-    imageSource:background,
-    title:"Product 2",
-    rating:5,
-    reviews:["review 1", "review 2"],
-    price:14000,
-    quantity:0,
-    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie ornare elit, in facilisis odio suscipit ut. Integer mollis quam id placerat auctor. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent vitae rhoncus sem, vel lobortis ipsum. In hac habitasse platea dictumst. Nulla porta volutpat nulla a vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
+    imageSource: background,
+    title: "Product 2",
+    rating: 5,
+    reviews: ["review 1", "review 2"],
+    price: 14000,
+    quantity: 0,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie ornare elit, in facilisis odio suscipit ut. Integer mollis quam id placerat auctor. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent vitae rhoncus sem, vel lobortis ipsum. In hac habitasse platea dictumst. Nulla porta volutpat nulla a vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
   },
   {
     id: 3,
-    imageSource:background,
-    title:"Product 3",
-    rating:5,
-    reviews:["review 1", "review 2"],
-    price:13000,
-    quantity:10,
-    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie ornare elit, in facilisis odio suscipit ut. Integer mollis quam id placerat auctor. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent vitae rhoncus sem, vel lobortis ipsum. In hac habitasse platea dictumst. Nulla porta volutpat nulla a vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
+    imageSource: background,
+    title: "Product 3",
+    rating: 5,
+    reviews: ["review 1", "review 2"],
+    price: 13000,
+    quantity: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie ornare elit, in facilisis odio suscipit ut. Integer mollis quam id placerat auctor. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent vitae rhoncus sem, vel lobortis ipsum. In hac habitasse platea dictumst. Nulla porta volutpat nulla a vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
   },
-]
+];
 
-const Home = () => (<>
-  <Container fluid className="landingImage">
-    <Row>
-      <h1>100% Natural Ingredients</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
-        voluptatibus.
-      </p>
-      <hr/>
-    </Row>
-  </Container>
-   <Container fluid>
-    <ProductList data={data} />
-  </Container>
-  </>
-);
+const Home = () => {
+  const ref = useRef(null);
+  const scrollToTop = () => ref.current?.scrollIntoView({behavior:'smooth'});
+  return (
+    <>
+      <Container ref={ref} fluid className="landingImage">
+        <Row>
+          <h1>100% Natural Ingredients</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+            voluptatibus.
+          </p>
+          <hr />
+        </Row>
+      </Container>
+      <Container fluid>
+        <ProductList data={data} />
+      </Container>
+      <div className="floatActionButton">
+        <Fab onClick={scrollToTop}>
+          <KeyboardArrowUp />
+        </Fab>
+      </div>
+    </>
+  );
+};
 
 export default Home;
